9544c2411ce37922314b62bddf81f55f4ad12e19
AnginAbroyan <anginabroyan@gmail.com>
2024-04-18 12:54:39 +0400

    Adjusted ecr

--- terraform/9-ecr.tf
+++ terraform/9-ecr.tf
@@ -1,7 +1,7 @@
 #ECR
 resource "aws_ecr_repository" "repository" {
   for_each = toset(var.ecr_repos)
-  name = each.key
+  name     = each.key
 }
 
 
@@ -12,8 +12,8 @@ resource "null_resource" "docker_image" {
     command = <<EOF
 	    aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin
 ${data.aws_caller_identity.current.account_id}.dkr.ecr.eu-central-1.amazonaws.com
-	    docker build -t "${aws_ecr_repository.repository[each.key].repository_url}:latest ${var.dockerfile_location}"
-	    docker push "${aws_ecr_repository.repository[each.key].repository_url}:latest"
+	    docker build -t "${aws_ecr_repository.repository[each.key].repository_url}:latest" ${var.dockerfile_location}
+	    docker push "${aws_ecr_repository.repository[each.key].repository_url}"
 	    EOF
   }
   triggers = {
