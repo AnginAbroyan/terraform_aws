97d46bb91640f39f35dca6fabd58c41a3b8e336f
AnginAbroyan <anginabroyan@gmail.com>
2024-04-18 12:18:00 +0400

    Adjusted deprecated docker_registry_image into docker_image

--- terraform/9-ecr.tf
+++ terraform/9-ecr.tf
@@ -5,7 +5,7 @@ resource "aws_ecr_repository" "repository" {
 }
 
 
-resource "docker_registry_image" "docker_image" {
+resource "docker_image" "docker_image" {
   for_each = toset(var.ecr_repos)
   name = "${aws_ecr_repository.repository[each.key].repository_url}:latest"
 
