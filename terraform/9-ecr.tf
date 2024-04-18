50dc1d79daf6307e3c128953d0e4f688775dcbaa
AnginAbroyan <anginabroyan@gmail.com>
2024-04-18 13:34:05 +0400

    Adjusted ecr

--- terraform/9-ecr.tf
+++ terraform/9-ecr.tf
@@ -9,13 +9,26 @@ resource "null_resource" "docker_image" {
   for_each = toset(var.ecr_repos)
 
   provisioner "local-exec" {
-    command = <<EOF
-	    aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin
-${data.aws_caller_identity.current.account_id}.dkr.ecr.eu-central-1.amazonaws.com
-	    docker build -t "${aws_ecr_repository.repository[each.key].repository_url}:latest" ${var.dockerfile_location}
-	    docker push "${aws_ecr_repository.repository[each.key].repository_url}"
+    interpreter = ["/bin/bash", "-c"]
+    command     = <<EOF
+                docker login --username AWS --password-stdin  `aws ecr get-login-password --region eu-central-1`
+                ${data.aws_caller_identity.current.account_id}.dkr.ecr.eu-central-1.amazonaws.com
+	            docker build -t ${aws_ecr_repository.repository[each.key].repository_url}:latest ${var.dockerfile_location}
+                docker push "${aws_ecr_repository.repository[each.key].repository_url}"
 	    EOF
   }
+  #    command = <<EOF
+  #	    aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin
+  #${data.aws_caller_identity.current.account_id}.dkr.ecr.eu-central-1.amazonaws.com
+  #	    docker build -t ${aws_ecr_repository.repository[each.key].repository_url}:latest ${var.dockerfile_location}
+  #	    docker push "${aws_ecr_repository.repository[each.key].repository_url}"
+  #	    EOF
+  #  }
+  //    command = "docker images && docker build -t ${aws_ecr_repository.repo.repository_url}:v1
+  //${path.module}/../../DOCKER/docker-sample-nginx/ && docker images && docker login --username
+  //AWS --password `aws ecr get-login-password --region us-east-1` 1####99292.dkr.ecr.us-east-1.amazonaws.com
+  //&& docker push ${aws_ecr_repository.repo.repository_url}:v1"
+
   triggers = {
     "run_at" = timestamp()
   }
