0a51fac232bde02f5a391094c50a5a41a701f9e2
AnginAbroyan <anginabroyan@gmail.com>
2024-04-19 00:33:49 +0400

    Updated asg

--- terraform/8-asg.tf
+++ terraform/8-asg.tf
@@ -22,56 +22,55 @@ resource "aws_launch_template" "launch_template" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.asg_sg.id]
   key_name               = var.instance_keypair
-#  user_data              = filebase64("${path.module}/user-data.sh")
+  user_data              = filebase64("${path.module}/user-data.sh")
   ebs_optimized          = true
   update_default_version = true
   iam_instance_profile {
     name = aws_iam_instance_profile.ec2_instance_profile.name
   }
 
-#  block_device_mappings {
-#    ebs {
-#      volume_size           = 8
-#      delete_on_termination = true
-#      volume_type           = "gp2"
-#    }
-#  }
-#  tag_specifications {
-#    resource_type = "instance"
-#    tags          = {
-#      Name = "test"
-#    }
-#  }
+  block_device_mappings {
+    ebs {
+      volume_size           = 8
+      delete_on_termination = true
+      volume_type           = "gp2"
+    }
+  }
+  tag_specifications {
+    resource_type = "instance"
+    tags          = {
+      Name = "test"
+    }
+  }
 }
 
 
 resource "aws_autoscaling_group" "asg" {
-  max_size          = var.asg_max_size
-  min_size          = var.asg_min_size
-  desired_capacity  = var.asg_desired_capacity
-  health_check_type = "EC2"
-  target_group_arns = [aws_lb_target_group.target_group.arn]
-  force_delete      = true
+  max_size                  = var.asg_max_size
+  min_size                  = var.asg_min_size
+  desired_capacity          = var.asg_desired_capacity
+  health_check_type         = "EC2"
+  target_group_arns         = [aws_lb_target_group.target_group.arn]
+  force_delete              = true
   health_check_grace_period = 300
   launch_template {
     id      = aws_launch_template.launch_template.id
-#    version = "$Latest"
     version = aws_launch_template.launch_template.latest_version
   }
-  vpc_zone_identifier       = aws_subnet.private_subnets[*].id
-  termination_policies      = ["OldestInstance"]
-#  wait_for_capacity_timeout = "10m"
+  vpc_zone_identifier  = aws_subnet.private_subnets.id
+  termination_policies = ["OldestInstance"]
+    wait_for_capacity_timeout = "10m"
   tag {
     key                 = "Name"
     value               = "${var.project_name}-ASG"
     propagate_at_launch = true
   }
-#
-#  tag {
-#    key                 = "Project"
-#    value               = "Terraform_Brainscale"
-#    propagate_at_launch = true
-#  }
+
+  tag {
+    key                 = "Project"
+    value               = "Terraform_Brainscale"
+    propagate_at_launch = true
+  }
 }
 
 ##TODO
