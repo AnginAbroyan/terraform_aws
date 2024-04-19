3c7db16853934cad4a3487d6ad96461926ee49ba
AnginAbroyan <anginabroyan@gmail.com>
2024-04-19 09:47:37 +0400

    Update

--- terraform/4-nat.tf
+++ terraform/4-nat.tf
@@ -6,9 +6,10 @@ resource "aws_eip" "nat_eip" {
 }
 
 resource "aws_nat_gateway" "nat_gtw" {
+  connectivity_type = "public"
   count         = length(var.private_subnet_cidrs)
   allocation_id = aws_eip.nat_eip[count.index].id
   subnet_id     = aws_subnet.public_subnets[count.index].id
-  depends_on = [aws_eip.nat_eip]
+  depends_on = [aws_internet_gateway.main_gateway]
   tags          = merge(var.tags, { Name = "${var.project_name}-nat-gtw-${count.index + 1}" })
 }
