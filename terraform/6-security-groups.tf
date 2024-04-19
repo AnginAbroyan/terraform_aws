0a2f9c9906f2b0b527537043fd4fa71b0987d126
AnginAbroyan <anginabroyan@gmail.com>
2024-04-19 09:11:02 +0400

    Update

--- terraform/6-security-groups.tf
+++ terraform/6-security-groups.tf
@@ -45,9 +45,15 @@ resource "aws_security_group" "security_group_private" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     from_port   = 22
     to_port     = 22
-    protocol    = "ssh"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
