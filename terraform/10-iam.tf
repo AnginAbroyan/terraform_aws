9c250cc8d30f0c992316d07818050944f71cf3d8
AnginAbroyan <anginabroyan@gmail.com>
2024-04-19 11:22:43 +0400

    Update

--- terraform/10-iam.tf
+++ terraform/10-iam.tf
@@ -1,3 +1,54 @@
+#resource "aws_iam_role" "ec2_role" {
+#  name = "${var.project_name}-EC2-Role"
+#  assume_role_policy = jsonencode({
+#    "Version" : "2012-10-17",
+#    "Statement": [
+#      {
+#        "Effect": "Allow",
+#        "Principal": {
+#          "Service": "ec2.amazonaws.com"
+#        },
+#        "Action": "sts:AssumeRole"
+#      }
+#    ]
+#  })
+#}
+#
+#
+#resource "aws_iam_policy" "ecr_policy" {
+#  name        = "${var.project_name}-ECR-Policy"
+#  description = "IAM policy for accessing ECR"
+#  policy      = jsonencode({
+#    "Version": "2012-10-17",
+#    "Statement": [
+#      {
+#        "Effect": "Allow",
+#        "Action": [
+#          "ecr:GetAuthorizationToken",
+#          "ecr:BatchCheckLayerAvailability",
+#          "ecr:GetDownloadUrlForLayer",
+#          "ecr:GetRepositoryPolicy",
+#          "ecr:DescribeRepositories",
+#          "ecr:ListImages",
+#          "ecr:DescribeImages",
+#          "ecr:BatchGetImage"
+#        ],
+#        "Resource": "*"
+#      }
+#    ]
+#  })
+#}
+#
+#resource "aws_iam_role_policy_attachment" "ec2_role_policy_attachment" {
+#  role       = aws_iam_role.ec2_role.name
+#  policy_arn = aws_iam_policy.ecr_policy.arn
+#}
+#
+#resource "aws_iam_instance_profile" "ec2_instance_profile" {
+#  name = "${var.project_name}-EC2-Instance-Profile"
+#  role = aws_iam_role.ec2_role.name
+#}
+
 resource "aws_iam_role" "ec2_role" {
   name = "${var.project_name}-EC2-Role"
   assume_role_policy = jsonencode({
@@ -13,7 +64,6 @@ resource "aws_iam_role" "ec2_role" {
     ]
   })
 }
-
 
 resource "aws_iam_policy" "ecr_policy" {
   name        = "${var.project_name}-ECR-Policy"
@@ -39,9 +89,46 @@ resource "aws_iam_policy" "ecr_policy" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ec2_role_policy_attachment" {
+resource "aws_iam_role_policy_attachment" "ecr_role_policy_attachment" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = aws_iam_policy.ecr_policy.arn
+}
+
+resource "aws_iam_role_policy" "cloudtrail_role_policy" {
+  name        = "${var.project_name}-CloudTrail-Policy"
+  role        = aws_iam_role.ec2_role.name
+  description = "IAM policy for CloudTrail"
+  policy      = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": "cloudtrail:Read",
+        "Resource": "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "iam_policy" {
+  name        = "${var.project_name}-IAM-Policy"
+  role        = aws_iam_role.ec2_role.name
+  description = "IAM policy for IAM"
+  policy      = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": "iam:Write",
+        "Resource": "*",
+        "Condition": {
+          "StringEquals": {
+            "iam:AWSServiceName": "replication.ecr.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
 }
 
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
