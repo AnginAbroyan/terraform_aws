6bae8a0c1aa4dbdf5403238770f22520f740ffc4
AnginAbroyan <anginabroyan@gmail.com>
2024-04-19 12:16:07 +0400

    Update

--- terraform/user-data.sh
+++ terraform/user-data.sh
@@ -12,7 +12,7 @@ $(aws ecr get-login --no-include-email --region eu-central-1)
 
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 767397941516.dkr.ecr.eu-central-1.amazonaws.com
 
-docker pull 767397941516.dkr.ecr.eu-central-1.amazonaws.com/brainscale-simple-app:1.0
+docker pull 767397941516.dkr.ecr.eu-central-1.amazonaws.com/ecr-brainscale:latest
 
-docker run -d -p 3000:3000 -e ENV_VAR=brainscale-simple-app 767397941516.dkr.ecr.eu-central-1.amazonaws.com/brainscale-simple-app:1.0
+docker run -d -p 3000:3000 -e ENV_VAR=brainscale-simple-app 767397941516.dkr.ecr.eu-central-1.amazonaws.com/ecr-brainscale:latest
 
