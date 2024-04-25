863233668e928d0cd44d28221d1776a5d26c8598
AnginAbroyan <anginabroyan@gmail.com>
2024-04-25 21:21:29 +0400

    Updated ECR

---
+++ terraform/12-launch-template.tf
@@ -0,0 +1,12 @@
+resource "aws_launch_template" "this" {
+  depends_on             = [aws_eip.nat_eip]
+  name                   = "${var.project_name}-tpl"
+  image_id               = var.instance_ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.security_group_private.id]
+  key_name               = var.instance_keypair
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ec2_instance_profile.name
+  }
+#  user_data = filebase64("${path.module}/user-data.sh")
+}
--- terraform/8-asg.tf
+++ terraform/8-asg.tf
@@ -1,16 +1,3 @@
-resource "aws_launch_template" "this" {
-  depends_on             = [aws_eip.nat_eip]
-  name                   = "${var.project_name}-tpl"
-  image_id               = var.instance_ami
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.security_group_private.id]
-  key_name               = var.instance_keypair
-  iam_instance_profile {
-    name = aws_iam_instance_profile.ec2_instance_profile.name
-  }
-  user_data = filebase64("${path.module}/user-data.sh")
-}
-
 resource "aws_autoscaling_group" "this" {
   name                      = "${var.project_name}-asg"
   max_size                  = var.asg_max_size
